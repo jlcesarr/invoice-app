4f81646bcd5d838ed3e10f66a4a698d999bc3453
Julio Cesar <jcesarpereira2021@gmail.com>
2022-11-25 07:41:06 -0300

    feat(tailwind): import fonts and colors tokens from D.S

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,51 @@
 module.exports = {
   content: ['src/**/*.tsx'],
   theme: {
-    extend: {}
+    fontSize: {
+      xs: 11,
+      sm: 12,
+      md: 16,
+      lg: 20,
+      xl: 32
+    },
+    colors: {
+      violet: {
+        primary: '#7C5DFA',
+        secondary: '#9277FF'
+      },
+      yankees: {
+        primary: '#1E2139',
+        secondary: '#252945'
+      },
+      lavender: {
+        primary: '#DFE3FA'
+      },
+      grey: {
+        primary: '#888EB0'
+      },
+      black: {
+        primary: '#0C0E16',
+        secondary: '#141625'
+      },
+      ube: {
+        primary: '#7E88C3'
+      },
+      opal: {
+        primary: '#EC5757'
+      },
+      pink: {
+        primary: '#FF9797'
+      },
+      white: {
+        primary: '#F8F8FB'
+      }
+    },
+    extend: {
+      fontFamily: {
+        'spartan-bold': 'Spartan Bold, sans-serif',
+        'spartan-medium': 'Spartan Medium, sans-serif'
+      }
+    }
   },
   plugins: []
 };
